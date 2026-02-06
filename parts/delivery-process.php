b3850a00002b9982e5c5e6c8e3fba0b5553f4628
agni-devG <akshatdx12345@gmail.com>
2026-02-06 15:51:56 +0530

    added an image

--- parts/delivery-process.php
+++ parts/delivery-process.php
@@ -8,7 +8,7 @@
         <h2>From Build to Production Floor</h2>
         <div class="delivery-inside">
             <div class="delivery-img">
-                <img src="<?php echo get_template_directory_uri() . '/assets/images/home/delivery-process.svg'; ?>" alt="">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/home/delivery-process-2.svg'; ?>" alt="">
             </div>
             <div class="delivery-content">
                 <p>Lorem ipsum dolor sit amet consectetur. Nullam integer lobortis convallis feugiat in risus eu purus malesuada. Pretium elementum ut consequat et id. Id dolor purus sit commodo sapien ligula. Non quis pellentesque ipsum morbi. Eget enim iaculis vitae ultrices et ornare eu. Convallis in ipsum nulla tempus quis accumsan eros rutrum. Cursus dictumst diam scelerisque placerat. Eget aenean semper sit ultricies non proin ultrices.</p>
